2d60c0db18f6cf486c9d00268e75ff2dcb589656
Majid <helloabdulmajid>
2022-04-07 03:29:44 +0530

    Add Nested Components & using props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import ExpenseItem from "./components/ExpenseItem";
-function App() {
-  let expenseDate = new Date(2022, 3, 30);
-  let expenseTitle = "School Fees";
-  let expenseAmount = 400;
+import Expenses from "./components/Expenses/Expenses";
+import React from "react";
+
+const App=()=> {
+  let expenses = [
+    {
+      id:'e1',
+      title:'Home Rent',
+      amount:250,
+      date:new Date(2022,12,3)
+    },
+    {
+      id:'e2',
+      title:"Electric Bill",
+      amount:355,
+      date:new Date(2022,2,7)
+    },
+    {
+      id:'e3',
+      title:"Petrol",
+      amount:114,
+      date:new Date(2022,5,5)
+    },
+    {
+      id:'e4',
+      title:"Books",
+      amount:700,
+      date:new Date(2022,7,1)
+    }
+  ]
+ 
   return (
     <div>
       <h3>Let's get Started</h3>
-      <ExpenseItem
-        date={expenseDate}
-        title={expenseTitle}
-        amount={expenseAmount}>
-      </ExpenseItem>
+      <Expenses item={expenses}/>
     </div>
   );
 }
---
+++ src/components/Expenses/ExpenseDate.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import './ExpenseDate.css';
+
+const ExpenseDate=(props)=>{
+    const month = props.date.toLocaleString("en-US", { month: "long" });
+    const year = props.date.getFullYear();
+    const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+    return(
+        <div className="expense_date">
+        <div className='expense_date_day'>{day}</div>
+        <div className='expense_date_month'>{month}</div>
+        <div className='expense_date_year'>{year}</div>
+        </div>
+    );
+  
+}
+
+
+export default ExpenseDate;
